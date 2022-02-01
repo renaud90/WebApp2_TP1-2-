319b03b64785caad7326753c8250c9bb76c3f5b7
Renaud Lussier <renaud_lussier90@hotmail.com>
2022-02-01 16:59:53 -0500

    First commit

---
+++ EvenementsAPI/Repository.cs
@@ -0,0 +1,15 @@
+﻿using EvenementsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvenementsAPI
+{
+    public class Repository
+    {
+        public static int IdSequenceEvenement { get; set; } = 1;
+        public static ISet<Evenement> Villes { get; set; } = new HashSet<Evenement>();
+
+    }
+}
