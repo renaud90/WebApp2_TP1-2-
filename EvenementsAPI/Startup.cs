9b005d62b2b710c3affa66d8f97aa7a928bd90ba
Renaud Lussier <renaud_lussier90@hotmail.com>
2022-02-02 15:34:46 -0500

    Merge villes_participations in develop

--- EvenementsAPI/Startup.cs
+++ EvenementsAPI/Startup.cs
@@ -31,6 +31,8 @@ namespace EvenementsAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IParticipationsBL,ParticipationsBL>();
+            services.AddScoped<IVillesBL,VillesBL>();
             services.AddScoped<ICategoriesBL, CategoriesBL>();
 
             services.AddControllers(o => { o.AllowEmptyInputInBodyModelBinding = true; o.Filters.Add<HtppResponseExceptionFilter>(); })
